8be8087a3bf14eaa5ede5406b0ebbb84b05fb021
lance <laclance@gmail.com>
2022-03-10 06:35:18 +0200

    Remove getToken secret as it is read automatically; Add git ignore stuff;

--- .next/static/webpack/webpack.2c3c912d4ee4a2db.hot-update.js
+++ .next/static/webpack/webpack.2c3c912d4ee4a2db.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "54393714a19f1520"; }
+/******/ 	__webpack_require__.h = function() { return "f4484d68705588f8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -1,9 +1,7 @@
 // This is an example of how to read a JSON Web Token from an API route
 import { getToken } from "next-auth/jwt";
 
-const secret = process.env.SECRET;
-
 export default async (req, res) => {
-  const token = await getToken({ req, secret });
+  const token = await getToken({ req });
   token ? res.status(200).json(token) : res.status(401);
 };
